3c203f34591a73ec91dd0d63b2baa0b0c9fb84fa
Adam Sattaur <hookaduckling@hotmail.com>
2015-11-14 17:46:28 +0000

    Show boss info as well as party info.

--- dl_habitica_info.sh
+++ dl_habitica_info.sh
@@ -5,10 +5,12 @@
 user_id=`cat user_id`
 api_key=`cat api_key`
 
+habitica_content=`curl -sX GET https://habitica.com:443/api/v2/content`
 party_info=`curl -sX GET -H "x-api-key:$api_key" -H "x-api-user:$user_id" https://habitica.com/api/v2/groups/party`
 
 #member_info=`echo $party_info | sed -E 's/"special":\{[^}]+\},//g' | sed -E 's/"challenges":\[\]//g' | sed -E 's/.+"members":\[([^]]+)\].+/\1/'`
 
 #members=`echo $member_info | grep -Eo '"(name|hp)":[^,]+'`
 
+echo $habitica_content
 echo $party_info
--- party_status.py
+++ party_status.py
@@ -5,20 +5,49 @@
 import json
 import sys
 
-party_json = sys.stdin.readline()
+# Config ---------------------------------------------------#
+max_health = 50			# Maximum player health
+health_bar_width = 10		# Width of player health bars
+boss_health_bar_width = 20	# Width of boss' health bar
+#-----------------------------------------------------------#
 
+content_json = sys.stdin.readline()
+party_json = sys.stdin.readline()
+habitica_content = json.loads(content_json)
 party_info = json.loads(party_json)
 
 messages = party_info["chat"]
 members = party_info["members"]
 
+# Draw a progress bar
+def progress_bar(value, max_value, width):
+	sys.stdout.write('[')
+	for i in range(int(round(width*value/max_value))):
+		sys.stdout.write('#')
+	for i in range(int(round(width*(max_value-value)/max_value))):
+		sys.stdout.write(' ')
+	sys.stdout.write(']')
+
 # Print names and health
+max_health = 50
+health_bar_width = 10
 for member in members:
 	health = member["stats"]["hp"]
-	sys.stdout.write('{}\t{}/50 '.format(member["profile"]["name"].ljust(15), int(round(health))))
-	sys.stdout.write('[')
-	for i in range(int(health/5)):
-		sys.stdout.write('#')
-	for i in range(int((50-health)/5)):
-		sys.stdout.write(' ')
-	sys.stdout.write(']\n')
+	sys.stdout.write('{}\t{}/{} '.format(member["profile"]["name"].\
+		ljust(15), int(round(health)), max_health))
+	progress_bar(health, max_health, health_bar_width)
+	sys.stdout.write('\n')
+
+# Print current boss and its health
+if "quest" in party_info and "key" in party_info["quest"]:
+	quest_key = party_info["quest"]["key"]
+	quest = habitica_content["quests"][quest_key]
+	if "boss" in quest:
+		boss_hp = party_info["quest"]["progress"]["hp"]
+		boss_max_hp = quest["boss"]["hp"]
+		boss_name = quest["boss"]["name"]
+		print '\nBoss: {}'.format(boss_name)
+		sys.stdout.write('{}/{} '.format(int(round(boss_hp)),\
+			boss_max_hp))
+		progress_bar(boss_hp, boss_max_hp, boss_health_bar_width)
+		sys.stdout.write('\n')
