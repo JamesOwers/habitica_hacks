e4d06cab1c9195310138ebd2eea7e7ed9144d503
Adam Sattaur <hookaduckling@hotmail.com>
2015-11-14 18:03:58 +0000

    Locally cache habitica content to improve speed.

--- dl_habitica_info.sh
+++ dl_habitica_info.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# Request party info from server using curl.
+# Request habitica info from server using curl.
 
 user_id=`cat user_id`
 api_key=`cat api_key`
 
-habitica_content=`curl -sX GET https://habitica.com:443/api/v2/content`
+if ! [ -e 'cache' ]; then
+	`mkdir cache`
+fi
+
+if ! [ -e 'cache/habitica_content.json' ]; then
+	`curl -sX GET https://habitica.com:443/api/v2/content > cache/habitica_content.json`
+fi
+habitica_content=`cat 'cache/habitica_content.json'`
 party_info=`curl -sX GET -H "x-api-key:$api_key" -H "x-api-user:$user_id" https://habitica.com/api/v2/groups/party`
-
-#member_info=`echo $party_info | sed -E 's/"special":\{[^}]+\},//g' | sed -E 's/"challenges":\[\]//g' | sed -E 's/.+"members":\[([^]]+)\].+/\1/'`
-
-#members=`echo $member_info | grep -Eo '"(name|hp)":[^,]+'`
 
 echo $habitica_content
 echo $party_info
