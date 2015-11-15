974afae11180d1c12dca19af69f301a42abe90e5
kungfujam <james.f.owers+github@gmail.com>
2015-11-15 01:24:06 +0000

    Added experience points
    Experience points are calculated by a formula only involving
    level. This is used to determine the denominator. Also tidied the
    formatting a bit. Hashtag pep8 yo.

--- party_status.py
+++ party_status.py
@@ -6,9 +6,10 @@ import json
 import sys
 
 # Config ---------------------------------------------------#
-max_health = 50			# Maximum player health
-health_bar_width = 10		# Width of player health bars
-boss_health_bar_width = 20	# Width of boss' health bar
+max_health = 50             # Maximum player health
+health_bar_width = 10       # Width of player health bars
+exp_bar_width = 10          # Width of player exp bars
+boss_health_bar_width = 20  # Width of boss' health bar
 #-----------------------------------------------------------#
 
 content_json = sys.stdin.readline()
@@ -19,35 +20,59 @@ party_info = json.loads(party_json)
 messages = party_info["chat"]
 members = party_info["members"]
 
+
 # Draw a progress bar
 def progress_bar(value, max_value, width):
-	sys.stdout.write('[')
-	for i in range(int(round(width*value/max_value))):
-		sys.stdout.write('#')
-	for i in range(int(round(width*(max_value-value)/max_value))):
-		sys.stdout.write(' ')
-	sys.stdout.write(']')
+    sys.stdout.write('[')
+    for i in range(int(round(width*value/max_value))):
+        sys.stdout.write('#')
+    for i in range(int(round(width*(max_value-value)/max_value))):
+        sys.stdout.write(' ')
+    sys.stdout.write(']')
+
 
 # Print names and health
 max_health = 50
 health_bar_width = 10
+sys.stdout.write('{}\t{}/{} {}\t{}/{}\n'.format(
+        "Name".ljust(15),
+        "hp",
+        "max",
+        " "*health_bar_width,
+        "exp".rjust(4),
+        "max".ljust(4)
+    )
+)
 for member in members:
-	health = member["stats"]["hp"]
-	sys.stdout.write('{}\t{}/{} '.format(member["profile"]["name"].\
-		ljust(15), int(round(health)), max_health))
-	progress_bar(health, max_health, health_bar_width)
-	sys.stdout.write('\n')
+    name = member["profile"]["name"]
+    lvl = member["stats"]["lvl"]
+    exp = member["stats"]["exp"]
+    max_exp = 0.25*lvl**2 + 10*lvl + 139.75
+    hp = member["stats"]["hp"]
+    sys.stdout.write('{}\t{}/{} '.format(
+            name.ljust(15),
+            int(round(hp)),
+            max_health
+        )
+    )
+    progress_bar(hp, max_health, health_bar_width)
+    sys.stdout.write('\t{}/{} '.format(
+            str(int(round(exp))).rjust(4),
+            str(int(round(max_exp))).ljust(4)
+        )
+    )
+    progress_bar(exp, max_exp, exp_bar_width)
+    sys.stdout.write('\n')
 
 # Print current boss and its health
 if "quest" in party_info and "key" in party_info["quest"]:
-	quest_key = party_info["quest"]["key"]
-	quest = habitica_content["quests"][quest_key]
-	if "boss" in quest:
-		boss_hp = party_info["quest"]["progress"]["hp"]
-		boss_max_hp = quest["boss"]["hp"]
-		boss_name = quest["boss"]["name"]
-		print '\nBoss: {}'.format(boss_name)
-		sys.stdout.write('{}/{} '.format(int(round(boss_hp)),\
-			boss_max_hp))
-		progress_bar(boss_hp, boss_max_hp, boss_health_bar_width)
-		sys.stdout.write('\n')
+    quest_key = party_info["quest"]["key"]
+    quest = habitica_content["quests"][quest_key]
+    if "boss" in quest:
+        boss_hp = party_info["quest"]["progress"]["hp"]
+        boss_max_hp = quest["boss"]["hp"]
+        boss_name = quest["boss"]["name"]
+        print '\nBoss: {}'.format(boss_name)
+        sys.stdout.write('{}/{} '.format(int(round(boss_hp)), boss_max_hp))
+        progress_bar(boss_hp, boss_max_hp, boss_health_bar_width)
+        sys.stdout.write('\n')
