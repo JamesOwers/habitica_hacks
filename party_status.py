1db40cb8951b249e31c907d8b1280eefe62a7fb8
kungfujam <james.f.owers+github@gmail.com>
2015-11-18 00:34:47 +0000

    Added level column to party status

--- party_status.py
+++ party_status.py
@@ -24,17 +24,17 @@ members = party_info["members"]
 # Print names, health and XP of party members.
 print printing.title("Party Status", 1)
 
-party_table = printing.table(["Name", "HP/Max HP", "XP/Max XP"])
+party_table = printing.table(["Name", "Level", "HP/Max HP", "XP/Max XP"])
 for member in members:
     name = member["profile"]["name"]
     lvl = member["stats"]["lvl"]
     exp = member["stats"]["exp"]
     max_exp = round((0.25*lvl**2 + 10*lvl + 139.75)/10)*10
     hp = member["stats"]["hp"]
-    party_table.add_row([name, \
-        str(int(round(hp))) + '/' + str(max_health) + ' ' + \
-            printing.progress_bar(hp, max_health, health_bar_width), \
-        str(int(round(exp))).rjust(4) + '/' + str(int(max_exp)).ljust(4) + ' ' + \
+    party_table.add_row([name, lvl,
+        str(int(round(hp))) + '/' + str(max_health) + ' ' +
+            printing.progress_bar(hp, max_health, health_bar_width),
+        str(int(round(exp))).rjust(4) + '/' + str(int(max_exp)).ljust(4) + ' ' +
             printing.progress_bar(exp, max_exp, exp_bar_width)])
 print party_table
 
